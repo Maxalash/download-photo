7caad72ad689a0aefab330864b140a2c0e7e342b
Makhmut <mahmudlyder@gmail.com>
2022-06-12 02:17:47 +0600

    fix: some fxes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function btn(event) {
   // let code = '47R2204';
+  document.getElementById('input_comp-kixfyipi').style.backgroundColor = 'red';
   const code = document.getElementById('input_comp-kixfyipi').value;
   let baseUrl = 'https://cloud-api.yandex.net/v1/disk/resources?path=';
 
@@ -16,7 +17,7 @@ function btn(event) {
     .then(res => {
       if(res.status>=400){
         document.getElementById('comp-kiz0dz15').style.visibility = 'visible';
-        document.getElementById('input_comp-kixfyipi').style.backgroundColor = 'red';
+        document.getElementById('comp-kiz0dz15').style.opacity = '1';
         return null;
       }else return res.json();
     })     
@@ -32,14 +33,24 @@ function btn(event) {
       }
     })
 
-    document.getElementById('input_comp-kixfyipi').value = ''
 }
 
 function balonmove(){
-  document.getElementById("ballon").style.width = '616px';
-  document.getElementById("ballon").style.height = '661px';
-
+  document.getElementById("ballon").style.width = '100%';
+  document.getElementById("ballon").style.height = '100%';
 }
+
+var input =  document.getElementById('input_comp-kixfyipi');
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    btn(event)
+  }
+});
+
+input.addEventListener('input',function(event) {
+  console.log('trigger');
+  input.style.backgroundColor = 'rgb(78, 212, 94)';
+})
 
 document.getElementById('down-btn').addEventListener('click',(ev)=>{ btn(ev)});
 
