9eb5ad73c432d62a32c04dec92a5c6946c22f6b6
Makhmut <mahmudlyder@gmail.com>
2022-06-22 01:24:40 +0600

    feat: add mobile menu button

--- index.js
+++ index.js
@@ -55,10 +55,30 @@ document.getElementById('comp-kik9a05i').addEventListener('click',(ev)=>{
   document.getElementById('comp-kkeizdxi').classList.add('emerge');
 })
 
+document.getElementById('dotmenu-before').addEventListener('click',(ev)=>{
+  document.getElementById('dotmenu-before').style.display = 'none';
+  document.getElementById('dotmenu-after').style.display = 'flex';
+  document.getElementById('whiteblur').style.display = 'block';
+  const items = document.getElementsByClassName('menu-item');
+  items[0].style.display = 'block';
+  items[1].style.display = 'block';
+
+});
+
+document.getElementById('dotmenu-after').addEventListener('click',(ev)=>{
+  document.getElementById('dotmenu-after').style.display = 'none';
+  document.getElementById('dotmenu-before').style.display = 'flex';
+  document.getElementById('whiteblur').style.display = 'none';
+  const items = document.getElementsByClassName('menu-item');
+  items[0].style.display = 'none';
+  items[1].style.display = 'none';
+});
+
 document.getElementById('down-btn').addEventListener('click',(ev)=>{ btn(ev)});
 
 document.getElementById('comp-kik9a05i').addEventListener('click', (eve) =>{
   document.getElementById("POPUPS_ROOT").style.display = 'block';
+  document.getElementById('comp-kik9a05i').style.display = 'none';
 });
 
 document.getElementById('comp-kipr32n41').addEventListener('click', (ev)=>{document.getElementById("POPUPS_ROOT").style.display = 'none';});
